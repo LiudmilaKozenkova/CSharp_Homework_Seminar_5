a59c4de6cd7fbc7a73b19509ab66758f4c2d1784
Liudmila <to_shagalova_lu@mail.ru>
2022-10-07 16:30:31 +0500

    task 3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -2,30 +2,19 @@
 // Найдите разницу между максимальным и минимальным
 // элементов массива. Например, [3 7 22 2 78] -> 76
 
-// 1. Метод получения псевдослучайного числа от min до max
-int GetIntValue(int min, int max)
+// 1. Создать и заполнить массив вещественных чисел
+double[] CreateFillArray(int size)
 {
-    return new Random().Next(min, max);
-}
-
-// 2. Создать массив
-int[] CreateArray(int size)
-{
-    return new int[size];
-}
-
-// 3. Заполнить массив случайными вещественными числами
-void Fill(int[] array, int min, int max)
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
     {
-        array[i] = GetIntValue(-100, 100);
+        array[i] = new Random().Next(0, 100) / 10.0;
     }
+    return array;
 }
 
-// 4. Распечатать массив
-void Print(int[] array)
+// 2. Распечатать массив
+void Print(double[] array)
 {
     int size = array.Length;
     for (int i = 0; i < size; i++)
@@ -35,11 +24,11 @@ void Print(int[] array)
     Console.WriteLine();
 }
 
-// 5. Найти max елемент массива
-int SearchMax(int[] data)
+// 3. Найти max елемент массива
+double SearchMax(double[] data)
 {
     int count = data.Length;
-    int max = data[0];
+    double max = data[0];
     for (int i = 0; i < count; i++)
     {
         if (data[i] > max) max = data[i];
@@ -47,11 +36,11 @@ int SearchMax(int[] data)
     return max;
 }
 
-// 6. Найти min элемент массива
-int SearchMin(int[] data)
+// 4. Найти min элемент массива
+double SearchMin(double[] data)
 {
     int count = data.Length;
-    int min = data[0];
+    double min = data[0];
     for (int i = 0; i < count; i++)
     {
         if (data[i] < min) min = data[i];
@@ -60,18 +49,12 @@ int SearchMin(int[] data)
 }
 
 // 7. Разница между max и min элементами массива
-int Diff(int[] array)
-{
-    int T = SearchMax(array)-SearchMin(array);
-    return T;
-}
-
-int[] array = CreateArray(GetIntValue(4, 6));
-Fill(array, -100, 100);
-Print(array);
+    
+double[] coll = CreateFillArray(5);
+Print(coll);
 Console.Write("Максимальное число = ");
-Console.WriteLine(SearchMax(array));
+Console.WriteLine(SearchMax(coll));
 Console.Write("Минимальное число = ");
-Console.WriteLine(SearchMin(array));
+Console.WriteLine(SearchMin(coll));
 Console.Write("Разница между max и min элементами массива = ");
-Console.WriteLine(Diff(array));
+Console.WriteLine(SearchMax(coll) - SearchMin(coll));
